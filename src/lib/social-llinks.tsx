c34fcc75dc46318520ffa356d256dea020f81261
Daksh-K-11 <khinvasara.daksh@gmail.com>
2026-02-16 18:31:21 +0530

    added social links

--- src/lib/social-llinks.tsx
+++ src/lib/social-llinks.tsx
@@ -8,22 +8,22 @@ import { Linkedin } from "lucide-react";
 const socialLinks = [
     {
       icon: Linkedin,
-      href: "https://linkedin.com",
+      href: "https://www.linkedin.com/in/daksh-khinvasara/",
       label: "LinkedIn",
     },
     {
       icon: siGithub,
-      href: "https://github.com",
+      href: "https://github.com/Daksh-K-11/",
       label: "GitHub",
     },
     {
       icon: siLeetcode,
-      href: "https://leetcode.com",
+      href: "https://leetcode.com/u/daksh1105/",
       label: "LeetCode",
     },
     {
       icon: siGeeksforgeeks,
-      href: "https://auth.geeksforgeeks.org",
+      href: "https://www.geeksforgeeks.org/profile/khinvasaywyg",
       label: "GeeksforGeeks",
     },
   ];
